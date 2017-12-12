5f8e5f9aee33aa9e217a83708d8f9a4c1ec9b07c
Victoria <vitalina27@gmail.com>
2017-12-12 06:08:00 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  def breed=(new_breed)
+    @jab=new_job
+  end
+
+  def breed
+    @breed
+  end
+end
