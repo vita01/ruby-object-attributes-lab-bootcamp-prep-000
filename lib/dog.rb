713db90950920e398ab9ea72d3168d835b942961
Victoria <vitalina27@gmail.com>
2017-12-12 06:09:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def name
     @name
   end
-
+ 
   def name=(new_name)
     @name = new_name
   end
   def breed=(new_breed)
-    @jab=new_job
+    @breed=new_job
   end
 
   def breed
